eaeaa16b8c7f44486a393207d550e61ad50cc83e
justinshwang <justinlongw@gmail.com>
2022-04-21 23:57:38 -0400

    added year_option param to run script and additional loop for year options

--- run.py
+++ run.py
@@ -19,21 +19,35 @@ def main():
     # Focus on Contributor for pie graphs
     data["Contributor"] = dict()
 
-    all_years = dict()
+    
     for lang in langs:
         dat_path = './census_interactive/data/raw/contributor_by_win/'+lang+'.csv'
         store_path = './census_interactive/data/processed/contributor'
+            
+        # All Years
+        year_option = "all"
+        all_years = dict()
         for compare_option in ["all", "male"]:
-            load_contributor_pie(lang, dat_path, store_path, compare_option)
+            load_contributor_pie(lang, dat_path, store_path, compare_option, year_option)
 
             # Save graph data to overall JSON data
             with open('./census_interactive/data/processed/contributor/'+lang+'.json') as json_file:
                 add_data = json.load(json_file)
                 all_years[lang] = add_data
-    data["Contributor"]["All Years"] = all_years
-    
-    single_year = dict()
-    
+            data["Contributor"]["All Years"] = single_year
+        
+        # Single Year
+        year_option = "single"
+        single_year = dict()
+        for compare_option in ["all", "male"]:
+            load_contributor_pie(lang, dat_path, store_path, compare_option, year_option)
+
+            # Save graph data to overall JSON data
+            with open('./census_interactive/data/processed/contributor/'+lang+'.json') as json_file:
+                add_data = json.load(json_file)
+                all_years[lang] = add_data
+            data["Contributor"]["Single Year"] = single_year
+        
     # Combine all JSON to one dictionary in js/
     with open('./census_interactive/single_graphs/Pie_Contributor_All/script/data.js', 'w') as out_file:
         out_file.write('var data = %s;' % json.dumps(data,indent=4, sort_keys=True))
