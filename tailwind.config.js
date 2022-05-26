7d5b42ac1c47cbcb6e5b06554fd30ffeebe18d64
Alexander Ma <ajm3@andrew.cmu.edu>
2022-05-26 14:58:59 -0400

    Basically finished index.html page and added some other important files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
