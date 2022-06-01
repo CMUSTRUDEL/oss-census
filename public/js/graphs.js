d429fc63659c51b87b6f21cb6103bd45483084c8
justinshwang <justinlongw@gmail.com>
2022-05-31 21:59:35 -0400

    completing graph tab changing function with placeholder img

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -16,13 +16,16 @@ function changeGraph() {
     for (let i = 0; i < 3; i++) {
         currType = types[i];
         currHTML = document.getElementById(currType);
+        currButton = document.getElementById(currType+"-button");
 
         if (currType == type) {
             // display selected graph type
             currHTML.setAttribute("style", "display:show");
+            currButton.setAttribute("class", "w-full h-full bg-primary hover:bg-purple-900 text-white text-lg font-semibold rounded-t-2xl")
         }
         else {
             currHTML.setAttribute("style", "display:none");
+            currButton.setAttribute("class", "w-full h-full bg-gray-300 hover:bg-gray-500 text-white text-lg font-semibold rounded-t-2xl")
         }
     }
 }
