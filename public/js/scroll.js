0e1473b0881998af78c8d0ced43cbea55eba7c22
justinshwang <justinlongw@gmail.com>
2022-06-27 18:10:55 -0400

    added scroll to bottom button

---
+++ public/js/scroll.js
@@ -0,0 +1,21 @@
+
+// Get the button
+let scrollButton = document.getElementById('btn-to-bottom');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  console.log(document.body.scrollTop)
+  if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+    scrollButton.style.display = 'block';
+  } else {
+    scrollButton.style.display = 'none';
+  }
+}
+
+function scrollToBottom(){
+  window.scrollTo(0, document.body.scrollHeight);
+}
