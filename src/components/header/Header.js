e21fb29cc74678f08e439744bc24f2268cb73581
An Tang <antang06211@gmail.com>
2023-04-18 14:14:32 -0400

    Header: Add background and Fix Width

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Button, Space } from "antd";
+import { Button, Divider, Space } from "antd";
 import Icon from '@ant-design/icons';
 
 import {ReactComponent as LogoIcon} from '../../assets/logo.svg';
 
 export default function Header(props) {
   return (
-    <Space 
-        size="large" 
-        style={{ 
-            position: "sticky",
-            top: 0,
-            zIndex: 1,
-            margin: "16px 24px",
-            float: "left",
+    <div style={{ 
+        position: "sticky",
+        top: 0,
+        zIndex: 1,
+        width: "100%",
+        background: "white",
     }}>
-        <Button type="text" icon={<Icon component={LogoIcon} />} />
-        <Button type="text">HOME</Button>
-        <Button type="text">QUICK FACTS</Button>
-        <Button type="text">GALLERY</Button>
-        <Button type="text">COMPARISONS</Button>
-        <Button type="text">REFERENCES</Button>
-    </Space>
+        <Space size="large" style={{margin: "16px 24px", float: "left"}}>
+            <Button type="text" icon={<Icon component={LogoIcon} />} />
+            <Button type="text">HOME</Button>
+            <Button type="text">QUICK FACTS</Button>
+            <Button type="text">GALLERY</Button>
+            <Button type="text">COMPARISONS</Button>
+            <Button type="text">REFERENCES</Button>
+        </Space>
+        <Divider style={{margin: "4px 0"}}/>
+    </div>
   );
 }
